d3f0e316bd7d006463a36671e0086700062ac5fe
Ritik Goel <rtogel56@gmail.com>
2025-11-30 12:28:26 +0530

    added favicon.ico

--- dist/middleware/ratelimiter.js
+++ dist/middleware/ratelimiter.js
@@ -7,7 +7,7 @@ exports.rateLimiter = void 0;
 const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 exports.rateLimiter = (0, express_rate_limit_1.default)({
     windowMs: 30 * 1000,
-    max: 100,
+    max: 10,
     message: "Too many requests from this IP, try again later.",
     standardHeaders: true,
     legacyHeaders: false,
--- src/middleware/ratelimiter.ts
+++ src/middleware/ratelimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
     windowMs: 30 * 1000,
-    max: 100,
+    max: 10,
     message: "Too many requests from this IP, try again later.",
     standardHeaders: true,
     legacyHeaders: false,
